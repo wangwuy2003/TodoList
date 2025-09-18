169156c904984a30fbfd2907cc52e525a38d8b1c
MacMini A6 <macminia6@MacMiniiiiiiiiiii.local>
2025-09-18 16:19:25 +0700

    update

--- TodoList/ViewModels/ListViewModel.swift
+++ TodoList/ViewModels/ListViewModel.swift
@@ -11,31 +11,43 @@ import SwiftUI
 
 /*
  CRUD FUNCTIONS
-    Create
-    Read
-    Update
-    Delete
+ Create
+ Read
+ Update
+ Delete
  */
 
 class ListViewModel: ObservableObject {
     
-    @Published var items: [ItemModel] = []
+    @Published var items: [ItemModel] = [] {
+        didSet {
+            saveItem()
+        }
+    }
+    
+    let itemsKey: String = "items_list"
     
     init() {
         getItems()
     }
     
     func getItems() {
-        let newItems = [
-            ItemModel(title: "This is the first item", isCompleted: false),
-            ItemModel(title: "This is the second item", isCompleted: false),
-            ItemModel(title: "This is the third item", isCompleted: false),
-            ItemModel(title: "This is the fourth item", isCompleted: false),
-            ItemModel(title: "This is the fifth item", isCompleted: false),
-            ItemModel(title: "This is the sixth item", isCompleted: false),
-            ItemModel(title: "This is the seventh item", isCompleted: false)
-        ]
-        items.append(contentsOf: newItems)
+        //        let newItems = [
+        //            ItemModel(title: "This is the first item", isCompleted: false),
+        //            ItemModel(title: "This is the second item", isCompleted: false),
+        //            ItemModel(title: "This is the third item", isCompleted: false),
+        //            ItemModel(title: "This is the fourth item", isCompleted: false),
+        //            ItemModel(title: "This is the fifth item", isCompleted: false),
+        //            ItemModel(title: "This is the sixth item", isCompleted: false),
+        //            ItemModel(title: "This is the seventh item", isCompleted: false)
+        //        ]
+        //        items.append(contentsOf: newItems)
+        guard let data = UserDefaults.standard.data(forKey: itemsKey),
+              let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data) else {
+            return
+        }
+        
+        self.items = savedItems
     }
     
     func deleteItem(at offsets: IndexSet) {
@@ -54,6 +66,12 @@ class ListViewModel: ObservableObject {
     func updateItem(item: ItemModel) {
         if let index = items.firstIndex(where: { $0.id == item.id }) {
             items[index] = item.updateCompletion()
+        }
+    }
+    
+    func saveItem() {
+        if let encodedData = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(encodedData, forKey: itemsKey)
         }
     }
 }
